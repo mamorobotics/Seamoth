4d2a61144108cd408fc1a0c6a44269c3fe29ea80
Dabuilda <107159084+TheMagnificentMiles@users.noreply.github.com>
2022-10-23 01:43:49 -0400

    Name changes

--- homebase.py
+++ homebase.py
@@ -1,14 +1,14 @@
-import seamoth
+import Seamoth
 
 def main():
-    conn = seamoth.DataConnection()
-    ui = seamoth.UI("staticImage.png")
+    conn = Seamoth.DataConnection()
+    ui = Seamoth.UI("staticImage.png")
 
     conn.serverStart(1951)
 
     while True:
         if conn.connected and len(conn.output) > 3:
-            ui.frame = seamoth.Camera.decode(conn.output)
+            ui.frame = Seamoth.Camera.decode(conn.output)
         
 
 if __name__ == "__main__":
--- submarine.py
+++ submarine.py
@@ -1,13 +1,13 @@
-import seamoth
+import Seamoth
 
 def main():
-    camera = seamoth.Camera()
-    conn = seamoth.DataConnection()
+    camera = Seamoth.Camera()
+    conn = Seamoth.DataConnection()
 
     conn.clientStart("192.168.86.42", 1951)
 
     while True:
-        conn.send(seamoth.Camera.encode(camera.readCameraData()))
+        conn.send(Seamoth.Camera.encode(camera.readCameraData()))
 
 if __name__ == "__main__":
     main()
