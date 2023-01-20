036a51adaf6ae8a6e94cba01bd8b8739e2a85bff
TheMagnificentMiles <107159084+TheMagnificentMiles@users.noreply.github.com>
2023-01-20 08:46:54 -0500

    RTD theme

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -16,14 +16,14 @@ author = 'TheMagnificentMiles'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc']
+extensions = ['sphinx.ext.autodoc', 'sphinx_rtd_theme']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # -- Options for HTML output -------------------------------------------------
 
-html_theme = 'pydata_sphinx_theme'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
 html_css_files = [
