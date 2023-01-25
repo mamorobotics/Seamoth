177ae95973c3cae9b6a28705163a0e7dc6e515bd
Dabuilda <107159084+TheMagnificentMiles@users.noreply.github.com>
2023-01-25 08:49:10 -0500

    Import fixes

--- submarine.py
+++ submarine.py
@@ -1,13 +1,13 @@
 import seamoth
 
 def main():
-    camera = Seamoth.Camera()
-    conn = Seamoth.DataConnection()
+    camera = seamoth.Camera()
+    conn = seamoth.DataConnection()
 
     conn.clientStart("192.168.86.31", 1951)
 
     while True:
-        conn.send(Seamoth.Camera.encode(camera.readCameraData(), 90))
+        conn.send(seamoth.Camera.encode(camera.readCameraData(), 90))
 
 if __name__ == "__main__":
     main()
