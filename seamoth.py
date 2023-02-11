17a38ab4775852b042c3695901201b178302ce91
Dabuilda <107159084+TheMagnificentMiles@users.noreply.github.com>
2023-02-10 22:28:04 -0500

    Default header param fix + docu changes

--- seamoth.py
+++ seamoth.py
@@ -33,7 +33,7 @@ class Controller:
 
     * **Controller Values** : ``controller.controllerValues`` = most recent values of the controller
 
-    :param controllerPort: Controller identifier number
+    :param controllerPort: controller identifier number
     """
 
     MAX_TRIG_VAL = float(256)
@@ -124,7 +124,7 @@ class Motor:
         """
         Assigns the motor to ports specified in the hardware map
 
-        :param name: The name of the motor in the hardware map
+        :param name: the name of the motor in the hardware map
         """
 
         if name in self.hardwareMap:
@@ -164,7 +164,7 @@ class Servo:
         """
         Assigns the servo to port specified in the hardware map
 
-        :param name: The name of the servo in the hardware map
+        :param name: the name of the servo in the hardware map
         """
 
         if name in self.hardwareMap:
@@ -176,7 +176,7 @@ class Servo:
         """
         Sets the position of the servo the function is called on.
 
-        :param position: Position of servo
+        :param position: position of servo
         """
 
         self.servo.value = position
@@ -213,7 +213,7 @@ class Camera:
         Encodes and compressed a Cv2 image to make it posssible to send over the internet
 
         :param image: Cv2 image object
-        :param quality: Quality of Jpeg compression
+        :param quality: quality of Jpeg compression
 
         :return: Compressed byte array representation of input image
         """
@@ -226,7 +226,7 @@ class Camera:
         """
         Decodes and decompresses an image encoded with *encode()*
 
-        :param image: Compressed byte array representation of image
+        :param image: compressed byte array representation of image
 
         :return: Cv2 image object
         """
@@ -240,10 +240,10 @@ class Camera:
         Resizes an image
 
         :param image: Cv2 image object
-        :param x: Image X
-        :param y: Image Y
+        :param x: image X
+        :param y: image Y
 
-        :return: Resized Cv2 image object
+        :return: resized Cv2 image object
         """
         return cv2.resize(image, (x, y), interpolation=cv2.INTER_AREA)
 
@@ -493,12 +493,12 @@ class DataConnection:
 
         return self.IP
 
-    def send(self, msg: bytearray, header: int = 0):
+    def send(self, msg: bytearray, header: int = 1):
         """
         Sends a message to all servers or clients connected to the program
 
         :param msg: message that you want to send in a byte form
-        :param header: message header value (default 0)
+        :param header: message header value (default 1)
         """
 
         send_length = str(len(msg)).encode('utf-8')
